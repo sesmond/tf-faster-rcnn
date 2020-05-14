ac1f057fdac1f7046a2e93475f5d1010341566cb
vincent <>
2020-05-14 19:00:24 +0800

    bugfix

--- experiments/scripts/train.sh
+++ experiments/scripts/train.sh
@@ -66,7 +66,7 @@ set -x
 
 if [ ! -f ${NET_FINAL}.index ]; then
   if [[ ! -z  ${EXTRA_ARGS_SLUG}  ]]; then
-    CUDA_VISIBLE_DEVICES=${GPU_ID} time\
+    CUDA_VISIBLE_DEVICES=${GPU_ID} time
     nohup python ./tools/trainval_net.py \
       --weight data/imagenet_weights/${NET}.ckpt \
       --imdb ${TRAIN_IMDB} \
@@ -79,7 +79,7 @@ if [ ! -f ${NET_FINAL}.index ]; then
       TRAIN.STEPSIZE ${STEPSIZE} ${EXTRA_ARGS} \
       >> ./logs/console_$Date.log 2>&1
   else
-    CUDA_VISIBLE_DEVICES=${GPU_ID} time \
+    CUDA_VISIBLE_DEVICES=${GPU_ID} time
     nohup python ./tools/trainval_net.py \
       --weight data/imagenet_weights/${NET}.ckpt \
       --imdb ${TRAIN_IMDB} \
