64dd47c98f78c028b5ebdb99dd13a2761fb841f8
vincent <>
2020-05-14 18:58:54 +0800

    bugfix

--- experiments/scripts/train.sh
+++ experiments/scripts/train.sh
@@ -66,8 +66,8 @@ set -x
 
 if [ ! -f ${NET_FINAL}.index ]; then
   if [[ ! -z  ${EXTRA_ARGS_SLUG}  ]]; then
-    nohup \
-    CUDA_VISIBLE_DEVICES=${GPU_ID} time python ./tools/trainval_net.py \
+    CUDA_VISIBLE_DEVICES=${GPU_ID} time\
+    nohup python ./tools/trainval_net.py \
       --weight data/imagenet_weights/${NET}.ckpt \
       --imdb ${TRAIN_IMDB} \
       --imdbval ${TEST_IMDB} \
@@ -77,10 +77,10 @@ if [ ! -f ${NET_FINAL}.index ]; then
       --net ${NET} \
       --set ANCHOR_SCALES ${ANCHORS} ANCHOR_RATIOS ${RATIOS} \
       TRAIN.STEPSIZE ${STEPSIZE} ${EXTRA_ARGS} \
-      >> ./logs/console_$Date.log 2>&1 
+      >> ./logs/console_$Date.log 2>&1
   else
-    nohup \
-    CUDA_VISIBLE_DEVICES=${GPU_ID} time python ./tools/trainval_net.py \
+    CUDA_VISIBLE_DEVICES=${GPU_ID} time \
+    nohup python ./tools/trainval_net.py \
       --weight data/imagenet_weights/${NET}.ckpt \
       --imdb ${TRAIN_IMDB} \
       --imdbval ${TEST_IMDB} \
