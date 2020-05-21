aae16ace641096bc98e63e21b0a4a00e80ee718a
vincent <>
2020-05-21 17:41:19 +0800

    bugfix

--- bin/train.sh
+++ bin/train.sh
@@ -55,18 +55,17 @@ RATIOS="[0.5,1,2]"
 LOG="experiments/logs/${NET}_${TRAIN_IMDB}_${EXTRA_ARGS_SLUG}_${NET}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
-
+set +x
 if [[ ! -z  ${EXTRA_ARGS_SLUG}  ]]; then
   NET_FINAL=output/${NET}/${TRAIN_IMDB}/${EXTRA_ARGS_SLUG}/${NET}_faster_rcnn_iter_${ITERS}.ckpt
 else
   NET_FINAL=output/${NET}/${TRAIN_IMDB}/default/${NET}_faster_rcnn_iter_${ITERS}.ckpt
 fi
-set +x
 
 if [ ! -f ${NET_FINAL}.index ]; then
   if [[ ! -z  ${EXTRA_ARGS_SLUG}  ]]; then
     CUDA_VISIBLE_DEVICES=${GPU_ID}
-#    nohup \
+    nohup \
     python ./tools/trainval_net.py \
       --weight data/imagenet_weights/${NET}.ckpt \
       --imdb ${TRAIN_IMDB} \
@@ -76,12 +75,12 @@ if [ ! -f ${NET_FINAL}.index ]; then
       --tag ${EXTRA_ARGS_SLUG} \
       --net ${NET} \
       --set ANCHOR_SCALES ${ANCHORS} ANCHOR_RATIOS ${RATIOS} \
-      TRAIN.STEPSIZE ${STEPSIZE} ${EXTRA_ARGS}
-#      >> ./logs/console_$Date.log 2>&1 &
+      TRAIN.STEPSIZE ${STEPSIZE} ${EXTRA_ARGS} \
+      >> ./logs/console_$Date.log 2>&1 &
 
   else
     CUDA_VISIBLE_DEVICES=${GPU_ID}
-#    nohup python \
+     nohup \
      python ./tools/trainval_net.py \
       --weight data/imagenet_weights/${NET}.ckpt \
       --imdb ${TRAIN_IMDB} \
@@ -90,8 +89,8 @@ if [ ! -f ${NET_FINAL}.index ]; then
       --cfg experiments/cfgs/${NET}.yml \
       --net ${NET} \
       --set ANCHOR_SCALES ${ANCHORS} ANCHOR_RATIOS ${RATIOS} \
-      TRAIN.STEPSIZE ${STEPSIZE} ${EXTRA_ARGS}
-#      >> ./logs/console_$Date.log 2>&1 &
+      TRAIN.STEPSIZE ${STEPSIZE} ${EXTRA_ARGS} \
+      >> ./logs/console_$Date.log 2>&1 &
 
   fi
 fi
